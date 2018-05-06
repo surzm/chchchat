2ce9aa15ad61261c55cac61ed2ae801a2f3bae50
Denis Ilyn <mariokyy@breadhead.ru>
2018-05-07 00:40:20 +0300

    message blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,5 +31,3 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
 
     return app
-
-    return app
---
+++ flaskr/message.py
@@ -0,0 +1,44 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from flaskr.db import get_db
+
+bp = Blueprint('message', __name__, url_prefix='/message')
+
+
+@bp.route('/', methods=('GET'))
+def post():
+    messages = []
+
+    return render_template('message/index.html', messages)
+
+
+@bp.route('/post', methods=('POST'))
+def post():
+    if request.method == 'POST':
+        body = request.form['body']
+        post_id = request.form['post_id']
+        db = get_db()
+        error = None
+
+        if body is None:
+            error = 'Text did not send'
+
+        if post_id:
+            if db.execute(
+                'SELECT id FROM message WHERE id = ?', (post_id)
+            ).fetchone() is None:
+                error = 'Post {} not found'.format(post_id),
+
+        if error is None:
+            db.execute(
+                'INSERT INTO message (body, user_id, reply_post_id, created_at) VALUES (?, ?, ?, ?)',
+                (body, session.get('user_id'), post_id, timestamp)
+                # timestamp - flaskr timestamp
+            )
+            db.commit()
+            return redirect(url_for('message.index'))
+
+        flash(error)
+
+    return render_template('message/index.html')
